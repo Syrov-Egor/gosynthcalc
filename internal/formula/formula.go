f5195d8611a43fb04b0c799212ef9e68c4978b1a
Syrov-Egor <syrov_ev@mail.ru>
2025-05-14 17:09:09 +0300

    Added MassPercent, AtomicPercent and OxidePercent functions

--- internal/formula/formula.go
+++ internal/formula/formula.go
@@ -1,20 +1,26 @@
 package formula
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 type ChemicalFormula struct {
 	formula       string
 	parsedFormula *[]Atom
 	molarMass     *float64
+	massPercent   *[]Atom
+	atomicPercent *[]Atom
+	oxidePercent  *[]Atom
 }
 
 func NewChemicalFormula(formula string) (*ChemicalFormula, error) {
-	validator := FormulaValidator{formula: formula}
+	newFormula := strings.Replace(formula, " ", "", -1)
+	validator := FormulaValidator{formula: newFormula}
 	err := validator.validate()
 	if err != nil {
 		return nil, err
 	}
-	newFormula := strings.Replace(formula, " ", "", -1)
 	return &ChemicalFormula{
 		formula: newFormula,
 	}, nil
@@ -35,8 +41,31 @@ func (c *ChemicalFormula) ParsedFormula() []Atom {
 
 func (c *ChemicalFormula) MolarMass() float64 {
 	if c.molarMass == nil {
-		mass := MolarMass{c.ParsedFormula()}.calcMolarMass()
+		mass := MolarMass{c.ParsedFormula()}.molarMass()
 		c.molarMass = &mass
 	}
 	return *c.molarMass
+}
+
+func (c *ChemicalFormula) MassPercent() []Atom {
+	if c.massPercent == nil {
+		percent := MolarMass{c.ParsedFormula()}.massPercent()
+		c.massPercent = &percent
+	}
+	return *c.massPercent
+}
+
+func (c *ChemicalFormula) AtomicPercent() []Atom {
+	if c.atomicPercent == nil {
+		percent := MolarMass{c.ParsedFormula()}.atomicPercent()
+		c.atomicPercent = &percent
+	}
+	return *c.atomicPercent
+}
+
+func (c *ChemicalFormula) OxidePercent() []Atom {
+	if c.oxidePercent == nil {
+		fmt.Println(MolarMass{c.ParsedFormula()}.customOxides("SO4", "K2O2"))
+	}
+	return []Atom{}
 }
