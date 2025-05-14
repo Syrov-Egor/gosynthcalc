7d0f3215b0f75e1c2c3591cc221a0cc03475220b
Syrov-Egor <syrov_ev@mail.ru>
2025-05-14 17:08:03 +0300

    Added UniqueElems and SumFloatS helper functions

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -7,3 +7,24 @@ func StringCounter(s string) map[string]int {
 	}
 	return counts
 }
+
+func UniqueElems(atomsList []string) []string {
+	seen := make(map[string]bool)
+	uniqueAtomsList := []string{}
+
+	for _, atom := range atomsList {
+		if !seen[atom] {
+			seen[atom] = true
+			uniqueAtomsList = append(uniqueAtomsList, atom)
+		}
+	}
+	return uniqueAtomsList
+}
+
+func SumFloatS(s []float64) float64 {
+	var sum float64 = 0.0
+	for _, el := range s {
+		sum += el
+	}
+	return sum
+}
