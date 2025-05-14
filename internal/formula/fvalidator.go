5afbb99d626f4b577425048a2f45d1552dd2da6e
Syrov-Egor <syrov_ev@mail.ru>
2025-05-14 14:31:42 +0300

    Done functions for validator

--- internal/formula/fvalidator.go
+++ internal/formula/fvalidator.go
@@ -2,26 +2,15 @@ package formula
 
 import (
 	"fmt"
-	"regexp"
 	"slices"
 	"sort"
 	"strings"
+
+	"github.com/Syrov-Egor/gosynthcalc/internal/utils"
 )
 
 type FormulaValidator struct {
-	formula        string
-	atomRegex      *regexp.Regexp
-	allowedSymbols *regexp.Regexp
-	letterRegex    *regexp.Regexp
-}
-
-func NewFormulaValidator(formula string) *FormulaValidator {
-	return &FormulaValidator{
-		formula:        formula,
-		atomRegex:      regexp.MustCompile(`([A-Z][a-z]*)`),
-		allowedSymbols: regexp.MustCompile(`[^A-Za-z0-9.({[)}\]*·•]`),
-		letterRegex:    regexp.MustCompile(`[a-z]`),
-	}
+	formula string
 }
 
 func (v FormulaValidator) emptyFormula() bool {
@@ -29,22 +18,15 @@ func (v FormulaValidator) emptyFormula() bool {
 }
 
 func (v FormulaValidator) invalidCharacters() []string {
-	return v.allowedSymbols.FindAllString(v.formula, -1)
+	return regexes.allowedSymbols.FindAllString(v.formula, -1)
 }
 
 func (v FormulaValidator) invalidAtoms() []string {
-	atoms := v.atomRegex.FindAllString(v.formula, -1)
+	atoms := regexes.atomRegex.FindAllString(v.formula, -1)
 	invalid := make([]string, 0)
 	cFormula := strings.Clone(v.formula)
-
-	seen := make(map[string]bool)
-	uniqueAtoms := []string{}
-	for _, atom := range atoms {
-		if !seen[atom] {
-			seen[atom] = true
-			uniqueAtoms = append(uniqueAtoms, atom)
-		}
-	}
+	slices.Sort(atoms)
+	uniqueAtoms := slices.Compact(atoms)
 	sort.Slice(uniqueAtoms, func(i, j int) bool {
 		return len(uniqueAtoms[i]) > len(uniqueAtoms[j])
 	})
@@ -55,8 +37,45 @@ func (v FormulaValidator) invalidAtoms() []string {
 		}
 		cFormula = strings.Replace(cFormula, atom, "", -1)
 	}
-	leftovers := v.letterRegex.FindAllString(cFormula, -1)
+	leftovers := regexes.letterRegex.FindAllString(cFormula, -1)
 	invalid = append(invalid, leftovers...)
-	fmt.Println(invalid)
 	return invalid
+}
+
+func (v FormulaValidator) bracketsBalance() bool {
+	counter := utils.StringCounter(v.formula)
+	for i := range len(regexes.openerBrackets) {
+		open := string(regexes.openerBrackets[i])
+		close := string(regexes.closerBrackets[i])
+		if counter[open] != counter[close] {
+			return false
+		}
+	}
+	return true
+}
+
+func (v FormulaValidator) numOfAdducts() int {
+	counter := utils.StringCounter(v.formula)
+	i := 0
+	for _, adduct := range regexes.adductSymbols {
+		i += counter[string(adduct)]
+	}
+	return i
+}
+
+func (v FormulaValidator) validate() error {
+	var err error
+	switch {
+	case v.emptyFormula():
+		err = fmt.Errorf("Empty formula string")
+	case len(v.invalidCharacters()) > 0:
+		err = fmt.Errorf("There are invalid characters in the formula: %s", v.invalidCharacters())
+	case len(v.invalidAtoms()) > 0:
+		err = fmt.Errorf("There are invalid atoms in the formula: %s", v.invalidAtoms())
+	case !v.bracketsBalance():
+		err = fmt.Errorf("Brackets %s %s are not balanced", string(regexes.openerBrackets), string(regexes.closerBrackets))
+	case v.numOfAdducts() > 1:
+		err = fmt.Errorf("There are more than 1 adduct symbol %s", string(regexes.adductSymbols))
+	}
+	return err
 }
