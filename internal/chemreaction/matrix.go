f632e5bdf1fc852af45cc6043d7e74f02f2bbea8
Syrov-Egor <syrov_ev@mail.ru>
2025-05-19 15:31:30 +0300

    createReacMatrix working

--- internal/chemreaction/matrix.go
+++ internal/chemreaction/matrix.go
@@ -6,6 +6,28 @@ import (
 )
 
 func createReacMatrix(parsedFormulas [][]chemformula.Atom) *mat.Dense {
-	matrix := mat.NewDense(3, 5, nil)
-	return matrix
+	atomMap := make(map[string]int)
+	var atomOrder []string
+
+	for _, formula := range parsedFormulas {
+		for _, atom := range formula {
+			if _, exists := atomMap[atom.Label]; !exists {
+				atomOrder = append(atomOrder, atom.Label)
+				atomMap[atom.Label] = len(atomOrder) - 1
+			}
+		}
+	}
+
+	numAtoms := len(atomOrder)
+	numFormulas := len(parsedFormulas)
+	data := make([]float64, numAtoms*numFormulas)
+	for formulaIdx, formula := range parsedFormulas {
+		for _, atom := range formula {
+			atomIdx := atomMap[atom.Label]
+			dataIdx := atomIdx*numFormulas + formulaIdx
+			data[dataIdx] = atom.Amount
+		}
+	}
+
+	return mat.NewDense(numAtoms, numFormulas, data)
 }
