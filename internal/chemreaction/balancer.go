8a667afc0b2d099fbcbe41e2d8a75bbd09e716e9
Syrov-Egor <syrov_ev@mail.ru>
2025-05-25 12:40:10 +0300

    Coefs reducing added

--- internal/chemreaction/balancer.go
+++ internal/chemreaction/balancer.go
@@ -1,6 +1,16 @@
 package chemreaction
 
-import "gonum.org/v1/gonum/mat"
+import (
+	"fmt"
+	"math/big"
+
+	"github.com/Syrov-Egor/gosynthcalc/internal/utils"
+	"gonum.org/v1/gonum/mat"
+)
+
+type Number interface {
+	int | int64 | float64
+}
 
 type Balancer struct {
 	reactionMatrix *mat.Dense
@@ -29,6 +39,68 @@ func NewBalancer(matrix *mat.Dense, separatorPos int, intify bool, tolerance ...
 	}
 }
 
-func (b *Balancer) intifyCoefs(coefs []float64, limit int) {
+func reduceCoefs(coefs []float64) ([]int64, error) {
+	rationals := make([]*utils.Rational, len(coefs))
+	for i, coef := range coefs {
+		rationals[i] = utils.NewRational(coef)
+		rationals[i].Simplify()
+	}
+	denominators := make([]*big.Int, len(rationals))
+	for i, rat := range rationals {
+		denominators[i] = new(big.Int).Set(rat.Den)
+	}
 
+	lcm := utils.FindLCMSlice(denominators)
+
+	intVals := make([]*big.Int, len(rationals))
+	for i, rat := range rationals {
+		quotient := new(big.Int).Div(lcm, rat.Den)
+		intVals[i] = new(big.Int).Mul(rat.Num, quotient)
+	}
+
+	gcd := utils.FindGCDSlice(intVals)
+
+	result := make([]int64, len(intVals))
+	for i, val := range intVals {
+		reduced := new(big.Int).Div(val, gcd)
+
+		if !reduced.IsInt64() {
+			return nil, fmt.Errorf("Result too large for int64: coefficient %d", i)
+		}
+
+		result[i] = reduced.Int64()
+	}
+
+	return result, nil
+}
+
+func intifyCoefs[Num Number](coefs []Num, limit int) []Num {
+	switch any(coefs).(type) {
+
+	case []float64:
+		float64Coefs := make([]float64, len(coefs))
+		for i, c := range coefs {
+			float64Coefs[i] = float64(c)
+		}
+		reduced, err := reduceCoefs(float64Coefs)
+		if err != nil {
+			return coefs
+		}
+		for _, coeff := range reduced {
+			if int(coeff) > limit {
+				return coefs
+			}
+		}
+		result := make([]Num, len(reduced))
+		for i, r := range reduced {
+			result[i] = Num(float64(r))
+		}
+		return result
+
+	case []int, []int64:
+		return coefs
+
+	default:
+		return coefs
+	}
 }
--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"math"
+	"math/big"
 )
 
 func StringCounter(s string) map[string]int {
@@ -36,4 +37,75 @@ func SumFloatS(s []float64) float64 {
 func RoundFloat(val float64, precision uint) float64 {
 	ratio := math.Pow(10, float64(precision))
 	return math.Round(val*ratio) / ratio
+}
+
+type Rational struct {
+	Num, Den *big.Int
+}
+
+func NewRational(f float64) *Rational {
+	if math.IsInf(f, 0) || math.IsNaN(f) {
+		return &Rational{big.NewInt(0), big.NewInt(1)}
+	}
+
+	rat := big.NewRat(0, 1)
+	rat.SetFloat64(f)
+
+	return &Rational{
+		Num: new(big.Int).Set(rat.Num()),
+		Den: new(big.Int).Set(rat.Denom()),
+	}
+}
+
+func (r *Rational) Simplify() {
+	gcd := new(big.Int).GCD(nil, nil, r.Num, r.Den)
+	r.Num.Div(r.Num, gcd)
+	r.Den.Div(r.Den, gcd)
+
+	if r.Den.Sign() < 0 {
+		r.Num.Neg(r.Num)
+		r.Den.Neg(r.Den)
+	}
+}
+
+func gcdBig(a, b *big.Int) *big.Int {
+	return new(big.Int).GCD(nil, nil, a, b)
+}
+
+func lcmBig(a, b *big.Int) *big.Int {
+	if a.Sign() == 0 || b.Sign() == 0 {
+		return big.NewInt(0)
+	}
+
+	gcd := gcdBig(a, b)
+	result := new(big.Int).Mul(a, b)
+	result.Div(result, gcd)
+	return new(big.Int).Abs(result)
+}
+
+func FindLCMSlice(nums []*big.Int) *big.Int {
+	if len(nums) == 0 {
+		return big.NewInt(1)
+	}
+
+	result := new(big.Int).Set(nums[0])
+	for i := 1; i < len(nums); i++ {
+		result = lcmBig(result, nums[i])
+	}
+	return result
+}
+
+func FindGCDSlice(nums []*big.Int) *big.Int {
+	if len(nums) == 0 {
+		return big.NewInt(1)
+	}
+
+	result := new(big.Int).Set(nums[0])
+	for i := 1; i < len(nums); i++ {
+		result = gcdBig(result, nums[i])
+		if result.Cmp(big.NewInt(1)) == 0 {
+			break
+		}
+	}
+	return result
 }
