0e17278c9327557c3c067faf266b570ac445687e
Syrov-Egor <syrov_ev@mail.ru>
2025-05-25 22:34:14 +0300

    Normilized coefs added

--- internal/chemreaction/reaction.go
+++ internal/chemreaction/reaction.go
@@ -1,20 +1,27 @@
 package chemreaction
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/Syrov-Egor/gosynthcalc/internal/chemformula"
+	"github.com/Syrov-Egor/gosynthcalc/internal/utils"
 	"gonum.org/v1/gonum/mat"
 )
 
+//TODO! test case "Fe2O3+C=Fe3O4+FeO+Fe+Fe3C+CO+CO2++++"
+
 type ChemicalReaction struct {
 	reaction       string
-	reacOpts       *ReacOptions
+	reacOpts       ReacOptions
 	decomposer     *reactionDecomposer
 	chemFormulas   *[]chemformula.ChemicalFormula
 	parsedFormulas *[][]chemformula.Atom
+	molarMasses    *[]float64
 	matrix         *mat.Dense
 	balancer       *Balancer
+	coefs          *MethodResult
+	normCoefs      *[]float64
 }
 
 type Mode int
@@ -59,8 +66,22 @@ func NewChemicalReaction(reaction string, options ...ReacOptions) (*ChemicalReac
 	return &ChemicalReaction{
 		reaction:   newReaction,
 		decomposer: decomp,
-		reacOpts:   &reacOpt,
+		reacOpts:   reacOpt,
 	}, nil
+}
+
+func (r *ChemicalReaction) calculatedTarget() (int, error) {
+	high := len(r.decomposer.products) - 1
+	low := -len(r.decomposer.reactants)
+	if r.reacOpts.target <= high && r.reacOpts.target >= low {
+		return r.reacOpts.target - low, nil
+	}
+	return -1, fmt.Errorf(
+		"The target integer %d should be in range %d : %d",
+		r.reacOpts.target,
+		low,
+		high,
+	)
 }
 
 func (r *ChemicalReaction) ChemFormulas() ([]chemformula.ChemicalFormula, error) {
@@ -95,19 +116,104 @@ func (r *ChemicalReaction) ParsedFormulas() ([][]chemformula.Atom, error) {
 	return *r.parsedFormulas, nil
 }
 
-func (r *ChemicalReaction) Matrix() *mat.Dense {
+func (r *ChemicalReaction) MolarMasses() ([]float64, error) {
+	if r.molarMasses == nil {
+		masses := make([]float64, len(r.decomposer.compounds))
+		formulas, err := r.ChemFormulas()
+		if err != nil {
+			return nil, err
+		}
+		for i, formula := range formulas {
+			masses[i] = formula.MolarMass()
+		}
+		r.molarMasses = &masses
+	}
+	return *r.molarMasses, nil
+}
+
+func (r *ChemicalReaction) Matrix() (*mat.Dense, error) {
 	if r.matrix == nil {
-		parsed, _ := r.ParsedFormulas()
+		parsed, err := r.ParsedFormulas()
+		if err != nil {
+			return nil, err
+		}
 		matrix := createReacMatrix(parsed)
 		r.matrix = matrix
 	}
-	return r.matrix
+	return r.matrix, nil
 }
 
-func (r *ChemicalReaction) Balancer() *Balancer {
+func (r *ChemicalReaction) Balancer() (*Balancer, error) {
+	mat, err := r.Matrix()
+	if err != nil {
+		return nil, err
+	}
 	if r.balancer == nil {
-		bal := NewBalancer(r.Matrix(), r.decomposer.separatorPos, r.reacOpts.intify, r.reacOpts.precision)
+		bal := NewBalancer(
+			mat,
+			r.decomposer.separatorPos,
+			r.reacOpts.intify,
+			r.reacOpts.precision,
+			r.reacOpts.tolerance)
 		r.balancer = bal
 	}
-	return r.balancer
+	return r.balancer, nil
+}
+
+func (r *ChemicalReaction) Coefficients() (*MethodResult, error) {
+	if r.coefs == nil {
+		parsed, err := r.ParsedFormulas()
+		if err != nil {
+			return nil, err
+		}
+		bal, err := r.Balancer()
+		if err != nil {
+			return nil, err
+		}
+		coeffs := Coeffs{
+			mode:               r.reacOpts.mode,
+			parsedFormulas:     parsed,
+			decomposedReaction: r.decomposer,
+			balancer:           bal,
+		}
+		coefs, err := coeffs.getCoeffs()
+		if err != nil {
+			return nil, err
+		}
+		r.coefs = &coefs
+	}
+	return r.coefs, nil
+}
+
+func (r *ChemicalReaction) NormCoefficients() ([]float64, error) {
+	if r.normCoefs == nil {
+		coefs, err := r.Coefficients()
+		if err != nil {
+			return nil, err
+		}
+		calc, err := r.calculatedTarget()
+		if err != nil {
+			return nil, err
+		}
+		targetCompound := coefs.Result[calc]
+		norm := make([]float64, len(coefs.Result))
+		for i, coef := range coefs.Result {
+			norm[i] = coef / targetCompound
+		}
+
+		norm = utils.RoundFloatS(norm, r.reacOpts.precision)
+		r.normCoefs = &norm
+	}
+	return *r.normCoefs, nil
+}
+
+func (r *ChemicalReaction) isBalanced() bool {
+	coefs, _ := r.Coefficients()
+	bal, _ := r.Balancer()
+	return isReactionBalanced(
+		bal.bAlgos.ReactantMatrix,
+		bal.bAlgos.ProductMatrix,
+		coefs.Result,
+		r.reacOpts.tolerance,
+	)
 }
