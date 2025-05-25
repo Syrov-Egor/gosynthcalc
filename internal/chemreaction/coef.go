36da37430b558d209f2d107a0190c7fe36a171ae
Syrov-Egor <syrov_ev@mail.ru>
2025-05-25 21:30:19 +0300

    Coeffs struct done

--- internal/chemreaction/coef.go
+++ internal/chemreaction/coef.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/Syrov-Egor/gosynthcalc/internal/chemformula"
+	"github.com/Syrov-Egor/gosynthcalc/internal/utils"
 )
 
 type Coeffs struct {
@@ -43,13 +44,63 @@ func (c *Coeffs) calculateCoeffs() (MethodResult, error) {
 }
 
 func (c *Coeffs) validateCoeffs(coefs []float64) error {
-	if !allPositive(coefs) {
-		return fmt.Errorf("Some coefs in %v are negative", coefs)
-	}
 	_, cols := c.balancer.reactionMatrix.Dims()
-	if len(coefs) != cols {
+
+	switch {
+	case !allPositive(coefs):
+		return fmt.Errorf("Some coefs in %v are negative or 0", coefs)
+	case len(coefs) != cols:
 		return fmt.Errorf("Number of coefs should be equal %d, got %d", cols, len(coefs))
+	default:
+		return nil
+	}
+}
+
+func (c *Coeffs) elementCountValidation() []string {
+	if c.mode != force {
+		r := make([]string, 0)
+		reactants := c.parsedFormulas[:c.balancer.separatorPos]
+		for _, reac := range reactants {
+			for _, atom := range reac {
+				r = append(r, atom.Label)
+			}
+		}
+
+		p := make([]string, 0)
+		products := c.parsedFormulas[c.balancer.separatorPos:]
+		for _, prod := range products {
+			for _, atom := range prod {
+				p = append(p, atom.Label)
+			}
+		}
+
+		ur := utils.UniqueElems(r)
+		up := utils.UniqueElems(p)
+
+		diff := utils.SymmetricDifference(ur, up)
+
+		return diff
+	}
+	return nil
+}
+
+func (c *Coeffs) getCoeffs() (MethodResult, error) {
+	user := "user"
+	nilStr := MethodResult{Method: user, Result: nil}
+
+	diff := c.elementCountValidation()
+	if diff != nil {
+		return nilStr,
+			fmt.Errorf("Cannot balance this reaction, because element(s) %v are only in one part of the reaction", diff)
+	}
+	coeffs, err := c.calculateCoeffs()
+	if err != nil {
+		return nilStr, err
+	}
+	err = c.validateCoeffs(coeffs.Result)
+	if err != nil {
+		return nilStr, err
 	}
 
-	return nil
+	return coeffs, nil
 }
